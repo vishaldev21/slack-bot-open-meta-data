742398c376317fa3c12aad3da96401590affbc53
unknown <Sandhya-kakunuri>
2026-04-24 23:38:27 +0530

    added: metadata-impact command

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,3 +5,5 @@ from .metadata_changes.recent_changes_context import (
 )
 
 from .orphan_entity.orphan_entity import get_orphan_entity
+
+from .metadata_impact.impact_analysis import get_impact_wrapper, get_impact
